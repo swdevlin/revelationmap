9addace11978284eda6ea74a1bf69f437e91958e
swdevlin <swdevlin@gmail.com>
2025-02-21 13:43:57 -0500

    Include sector and system name with route data

--- ruby/app/controllers/route_controller.rb
+++ ruby/app/controllers/route_controller.rb
@@ -1,9 +1,17 @@
 class RouteController < ApplicationController
 
   def index
-    hexes = Route.all.order("year, day, ship_id").map do |route|
+    hexes = Route
+                 .joins("LEFT JOIN sector ON sector.x = route.sector_x AND sector.y = route.sector_y")
+                 .joins("LEFT JOIN solar_system ON solar_system.x = route.hex_x AND solar_system.y = route.hex_y AND solar_system.sector_id = sector.id")
+                 .select("route.*, sector.name AS sector_name, solar_system.name AS solar_system_name")
+                 .all.order("year, day, ship_id")
+                 .map do |route|
       route.as_json.merge(origin_x: calculate_origin_x(route), origin_y: calculate_origin_y(route))
     end
+    # hexes = Route.all.order("year, day, ship_id").map do |route|
+    #
+    # end
     render json: hexes
   end
 
--- ruby/app/models/route.rb
+++ ruby/app/models/route.rb
@@ -2,4 +2,5 @@ class Route < ApplicationRecord
   self.table_name = "route"
 
   validates :ship_id, :year, :day, :sector_x, :sector_y, :hex_x, :hex_y, presence: true
+  belongs_to :sector, foreign_key: [:sector_x, :sector_y], primary_key: [:x, :y], optional: true
 end
